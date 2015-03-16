368dc2f12d25654e57be6f8ebd110ee154c4819f
edmundophie <edmund.ophie@yahoo.com>
2015-03-16 17:41:44 +0700

    Encryption done and tested

--- src/com/edmundophie/encryption/Encrypt.java
+++ src/com/edmundophie/encryption/Encrypt.java
@@ -48,32 +48,40 @@ public class Encrypt {
     }
     
     public void doEncrypt() {
+        ciphertext = "";
         int leftBlock = 0, rightBlock = 0;
         for(int len=0;len<plaintext.length();len+=2) {
             if(len==0) { // First block
                 leftBlock = (int) plaintext.charAt(len);
                 rightBlock = (int) plaintext.charAt(len+1);
             }
-            else { // xor with previous block
+            else { // xor with previous block (CBC)
                 leftBlock = leftBlock ^ (int) plaintext.charAt(len);
                 if(len==plaintext.length()-1) // handle padding block
                     rightBlock = rightBlock ^ 0;
                 else
                     rightBlock = rightBlock ^ (int) plaintext.charAt(len+1);
             }
-            int seed = key.charAt(len);
+            int seed = key.charAt(len/2);
+            int oldLeftBlock = 0, oldRightBlock = 0;
             for(int i=0;i<4;++i) {
+                if(i>0) { // xor kan dengan blok sebelumnya
+                    leftBlock = leftBlock ^ oldLeftBlock;
+                    rightBlock = rightBlock ^ oldRightBlock;
+                }
+                oldLeftBlock = leftBlock;
+                oldRightBlock = rightBlock;
                 for(int j=0;j<3;++j) {
                     // Feistel network
                     int temp = rightBlock;
-//                    rightBlock = mapBySBox(leftBlock, seed);
+                    rightBlock = mapBySBox(leftBlock, seed);
                     leftBlock = temp;
-                    
                     // 1x shift-left seed (wrapping bit)
-                    int msb = (seed&128)>>7; // ek unsigned atau signed int
+                    int msb = seed>>7; 
                     seed = (seed << 1) | msb;
                 }
             }
+            
             ciphertext += (char)leftBlock;
             ciphertext += (char)rightBlock;
         }
@@ -82,21 +90,43 @@ public class Encrypt {
     private int mapBySBox(int block, int seed) {
         Random rand = new Random(seed);
         Integer SBox[][] = new Integer[16][16];
+        
+        // Inisialisasi isi SBox dari 0-255
+        int counter = 0;
         for(int row=0;row<16;++row)
             for(int col=0;col<16;++col) {
-                SBox[row][col] = rand.nextInt()%256;
+                SBox[row][col] = counter;
+                ++counter;
             }
+        
+        // Create SBox using random seed (Knuth-Fisher-Yates algorithm)
+        int maxRow = 15;
+        for(int row=0;row<16;++row) {
+            int maxCol = 15;
+            for(int col=0;col<16;++col) {
+                int r = rand.nextInt(maxRow+1);
+                int c = rand.nextInt(maxCol+1);
+                
+                // Swap
+                int temp = SBox[maxRow][maxCol]; 
+                SBox[maxRow][maxCol] = SBox[r][c];
+                SBox[r][c] = temp;
+                maxCol--;
+            }
+            maxRow--;
+        }
         
         return SBox[block>>4][block&15];
     }
     
     public static void main(String args[]) {
+//        int d = mapBySBox(97, 5);
         String aa = "aa";
         byte a = new Byte("1");
         int c = a>>4 & 15;
         int intA = 'a';
         aa += 'a';
-        System.out.println("inta : " + aa);
-        System.out.println("c : " + c);
+        Random rand = new Random();
+        System.out.println("inta : " + rand.nextInt(1));
     }
 }
